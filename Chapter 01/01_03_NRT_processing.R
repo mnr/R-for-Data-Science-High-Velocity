60339719b8f7f22f7f3858854059631355715c2e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-04-18 13:39:30 -0700

    changed to run with old simulator and new acquisition .R

--- Chapter 01/01_03_NRT_processing.R
+++ Chapter 01/01_03_NRT_processing.R
@@ -8,30 +8,31 @@
 # an example of NRT processing high velocity data
  
 # Setup -------------------------------------------------------------------
-library(lubridate)
-HighVelSimTxt <- "HighVelocitySimulation.txt" # set this to the pathname of the simulation file
+library(DBI)
+library(RSQLite)
 
-cumulativeData <- data.frame("V1" = NA,
-                             "V2" = NA,
-                             "V3" = NA)
+mySQLiteDB <- dbConnect(RSQLite::SQLite(), 
+                        "AcquisitionDB.SQLite",
+                        flags = SQLITE_RO)
+
+# Process -----------------------------------------------------------------
+doThisSQL <- " SELECT avg(V3), count(*) FROM 'Acquired Data' "
 
 while (TRUE) {
-  # Acquire -----------------------------------------------------------------
-  # grab unprocessed data
-  
-  cumulativeData <- read.table(HighVelSimTxt)
-  
-  # Process -----------------------------------------------------------------
-  # do something with the data
-  meanResult <- mean(cumulativeData$V3, na.rm = TRUE)
+  dataRead <- dbGetQuery(mySQLiteDB, doThisSQL)
   
   # Present -----------------------------------------------------------------
   flush.console()
   cat(sprintf("With %d observations, the mean of V3 is %.3f",
-         nrow(cumulativeData),
-         meanResult),
+              dataRead$`count(*)`,
+         dataRead$`avg(V3)`),
       "\r"
   )
+  Sys.sleep(3) # because SQLite doesn't handle concurrency very well
 
 }
+
+# Clean up ----------------------------------------------------------------
+
+dbDisconnect(mySQLiteDB)
 
