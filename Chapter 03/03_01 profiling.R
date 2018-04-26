a53b27d4c3144ba8f11c9bea5617f62eb0a1aff3
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-04-25 23:05:03 -0700

    initial commits

---
+++ Chapter 03/03_01 profiling.R
@@ -0,0 +1,84 @@
+# Copyright Mark Niemann-Ross, 2018
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: R Programming in Data Science: High Velocity Data 
+
+# learning about Rprof for profiling
+
+
+# Setup -------------------------------------------------------------------
+# install.packages("profvis")
+library(lubridate)
+library(profvis)
+
+HighVelSimTxt <- "../HighVelocitySimulation.txt" # set this to the pathname of the simulation file
+
+# collectOneSecond with rbind and data frames acquire ------------------------------------------------------
+collectOneSecond_df <- function() {
+  oneSecData <- data.frame("V1" = NA,
+                           "V2" = NA,
+                           "V3" = NA)
+  
+  amountOfRunTime <- now() + seconds(1)
+  
+  while (amountOfRunTime > now()) {
+    newData <-
+      tryCatch(
+        read.table(HighVelSimTxt),
+        error = function(e)
+          NULL
+      )
+    oneSecData <- rbind(oneSecData, newData)
+  }
+  
+  return(oneSecData)
+}
+
+NonOptimizedResults <- collectOneSecond_df()
+
+
+# Optimized collectOneSecond ----------------------------------------------
+
+collectOneSecond_opt <- function() {
+  #  oneSecData <- data.frame("V1" = NA, "V2" = NA, "V3" = NA)
+  # pre-allocating a list is faster than copying each time through the loop
+  oneSecData <- vector(mode = "list", 10000)
+  
+  amountOfRunTime <- now() + seconds(1)
+  
+  dataIDX <- 1
+  
+  while (amountOfRunTime > now()) {
+    newData <-
+       scan( file = HighVelSimTxt, 
+            what = list( 0, "", "double" ),
+            nmax = 1,
+            fill = TRUE,
+            quiet = TRUE
+      )
+    oneSecData[[dataIDX]] <- newData
+    dataIDX <- dataIDX + 1
+  }
+  allGoodData <- oneSecData[!sapply(oneSecData, is.null)]
+  
+  return(allGoodData)
+}
+
+optimizedResults <- collectOneSecond_opt()
+
+# use profvis to generate profile graphs ----------------------------------
+profvis(collectOneSecond_df())
+profvis(collectOneSecond_opt())
+
+# use Rprof to generate profiles ----------------------------------------------------------------------
+
+Rprof("profileW_df") ; collectOneSecond_df() ; Rprof(NULL)
+Rprof("profileWOUT_df") ; collectOneSecond_opt() ; Rprof(NULL)
+
+# examine results of Rprof ---------------------------------------------------------
+
+summaryRprof("profileW_df")
+summaryRprof("profileWOUT_df")
+
---
+++ Chapter 03/03_02_startingScript.R
@@ -0,0 +1,43 @@
+
+# remarks -----------------------------------------------------------------
+
+# Copyright Mark Niemann-Ross, 2018
+# Author: Mark Niemann-Ross. mark.niemannross@gmail.com
+# LinkedIn: https://www.linkedin.com/in/markniemannross/
+# Github: https://github.com/mnr
+# More Learning: http://niemannross.com/link/mnratlil
+# Description: R Programming in Data Science: High Velocity Data 
+# Faster R for high velocity data - optimizing for high velocity data
+
+
+# setup -------------------------------------------------------------------
+# start up the high velocity data simulator
+HighVelSimTxt <- "HighVelocitySimulation.txt" # set this to the pathname of the simulation data
+
+
+# first attempt with no optimization ---------------------------------------------------------
+
+collectOneSecond <- function() {
+  oneSecData <- data.frame("V1" = NA,
+                           "V2" = NA,
+                           "V3" = NA)
+  
+  amountOfRunTime <- now() + seconds(1)
+  
+  while (amountOfRunTime > now()) {
+    newData <-
+      tryCatch(
+        read.table(HighVelSimTxt),
+        error = function(e)
+          NULL
+      )
+    oneSecData <- rbind(oneSecData, newData)
+  }
+  
+  return(oneSecData)
+}
+
+oneSecondOfData <- collectOneSecond()
+
+profvis(collectOneSecond_df())
+
