ad97f169f3440e9fbf49bcc3ae3707b2e75eef60
Mark Niemann-Ross <mark.niemannross@gmail.com>
2018-07-18 22:51:53 -0700

    fix path name to high velocity simulation .txt

--- Chapter 02/02_01_pollingForData.R
+++ Chapter 02/02_01_pollingForData.R
@@ -7,13 +7,13 @@
 
 # A demonstration of polling for data
 
-HighVelSimTxt <- "../HighVelocitySimulation.txt" # set this to the pathname of the simulation data
+HighVelSimTxt <- "HighVelocitySimulation.txt" # set this to the pathname of the simulation data
 
 while (TRUE) {
   if (file.exists(HighVelSimTxt)) {
     Sys.sleep(1) # give the simulator time to finish writing
     dataFromPolling <- read.table(HighVelSimTxt)
     print(dataFromPolling)
-    file.remove(readFromHere)
+    file.remove(HighVelSimTxt)
   }
 }
